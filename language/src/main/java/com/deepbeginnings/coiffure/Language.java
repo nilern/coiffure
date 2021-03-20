7a91703f9225c0b7a45e9cf9e8e075c5a7656ce1
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2021-03-20 23:10:42 +0200

    Don't macroexpand yet, after all

--- language/src/main/java/com/deepbeginnings/coiffure/Language.java
+++ language/src/main/java/com/deepbeginnings/coiffure/Language.java
@@ -3,10 +3,8 @@ package com.deepbeginnings.coiffure;
 import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.TruffleLanguage;
-import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.source.Source;
 
-import clojure.lang.Compiler;
 import clojure.lang.LispReader;
 
 import java.io.PushbackReader;
@@ -18,12 +16,6 @@ import java.util.List;
 public final class Language extends TruffleLanguage<Context> {
     public static final String ID = "coiffure";
     public static final String MIME_TYPE = "application/clojure";
-
-    static Object shallow_macroexpand(Object form) {
-        Object form_ = Compiler.macroexpand1(form);
-        return (form_ != form) ? shallow_macroexpand(form_) : form_;
-    }
-
 
     @Override
     protected Context createContext(Env env) {
@@ -37,8 +29,6 @@ public final class Language extends TruffleLanguage<Context> {
 
         PushbackReader reader = new PushbackReader(source.getReader());
         Object form = LispReader.read(reader, null);
-
-        form = shallow_macroexpand(form);
 
         Expr expr = Analyzer.analyze(form);
 
