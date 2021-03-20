90d49e77d0c0f87c473e25273a44f473c9470dee
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2021-03-20 15:47:03 +0200

    Add echoing REPL

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,13 @@
+import clojure.lang.LispReader
+import java.io.InputStreamReader
+import java.io.PushbackReader
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val reader = PushbackReader(InputStreamReader(System.`in`))
+
+    while (true) {
+        print("> ")
+        val form: Any = LispReader.read(reader, null)
+        println(form)
+    }
 }
