2f169698a89d412df00a49efd902576353ca0ef3
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2021-03-20 16:49:34 +0200

    Add some macroexpansion and Source sketches

--- src/main/kotlin/Coiffure.kt
+++ src/main/kotlin/Coiffure.kt
@@ -1,4 +1,9 @@
+/*import clojure.lang.LispReader
+import com.oracle.truffle.api.CallTarget*/
 import com.oracle.truffle.api.TruffleLanguage
+/*import com.oracle.truffle.api.source.Source
+import java.io.PushbackReader
+import java.io.StringReader*/
 
 @TruffleLanguage.Registration(id = "coiffure", name = "Clojure", implementationName = "Coiffure",
     defaultMimeType = Coiffure.MIME_TYPE,
@@ -7,6 +12,14 @@ class Coiffure : TruffleLanguage<Context>() {
     companion object {
         const val MIME_TYPE = "application/clojure"
     }
-    
+
     override fun createContext(env: Env?): Context = Context(env!!)
+
+    /*override fun parse(request: ParsingRequest?): CallTarget {
+        val source: Source? = request?.source
+        val argNames: List<String>? = request?.argumentNames
+        val reader = PushbackReader(source?.reader)
+        val form: Any = LispReader.read(reader, null)
+
+    }*/
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,26 @@
 import clojure.lang.LispReader
 import java.io.InputStreamReader
 import java.io.PushbackReader
+import clojure.lang.Compiler as ClojureCompiler
+
+/*import org.graalvm.polyglot.Context
+import org.graalvm.polyglot.Source*/
+
+fun shallow_macroexpand(form: Any?): Any? {
+    val form_ = ClojureCompiler.macroexpand1(form)
+    return if (form_ !== form) { shallow_macroexpand(form_) } else { form_ }
+}
 
 fun main(args: Array<String>) {
+    /*val context = Context.newBuilder("coiffure").`in`(System.`in`).`out`(System.`out`).build()
+    val source = Source.newBuilder("coiffure", InputStreamReader(System.`in`), "<stdin>").build()*/
+
     val reader = PushbackReader(InputStreamReader(System.`in`))
 
     while (true) {
         print("> ")
-        val form: Any = LispReader.read(reader, null)
-        println(form)
+        var form: Any? = LispReader.read(reader, null)
+        form = shallow_macroexpand(form)
+        println(form.toString() + " : " + form?.javaClass)
     }
 }
