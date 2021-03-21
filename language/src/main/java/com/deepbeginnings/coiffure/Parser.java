83687067c1dd7cdc087793e3fbd9941552e78170
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2021-03-21 13:54:43 +0200

    Read (simple) symbols

--- language/src/main/java/com/deepbeginnings/coiffure/Parser.java
+++ language/src/main/java/com/deepbeginnings/coiffure/Parser.java
@@ -2,6 +2,7 @@ package com.deepbeginnings.coiffure;
 
 import clojure.lang.IPersistentCollection;
 import clojure.lang.PersistentList;
+import clojure.lang.Symbol;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -38,9 +39,42 @@ final class Parser {
 
         case '(': return readList();
 
-        default: throw new IOException("TODO: parser lookahead '" + (char) input.peek() + "'");
+        default: return Symbol.intern(readIdentifier());
         }
     }
+
+    private long readInt() throws IOException {
+        long n = 0;
+        loop:
+        while (true) {
+            switch (input.peek()) {
+            case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+                n = 10 * n + Character.digit(input.read(), 10);
+                break;
+            default: break loop;
+            }
+        }
+        return n;
+    }
+
+    private String readIdentifier() throws IOException {
+        if (isSymbolStart(input.peek())) {
+            StringBuilder cs = new StringBuilder();
+            cs.append(input.read());
+
+            while (isSymbolPart(input.peek())) { cs.append(input.read()); }
+
+            return cs.toString();
+        } else {
+            throw new IOException("Invalid symbol start: '" + (char) input.peek() + "'");
+        }
+    }
+
+    private static boolean isSymbolStart(int c) {
+        return c != -1 && !Character.isDigit(c) && !Character.isWhitespace(c);
+    }
+
+    private static boolean isSymbolPart(int c) { return c != -1 && !Character.isWhitespace(c); }
 
     private IPersistentCollection readList() throws IOException {
         ArrayList<Object> forms = new ArrayList<>();
@@ -54,18 +88,5 @@ final class Parser {
             coll = coll.cons(formsIt.previous());
         }
         return coll;
-    }
-
-    private long readInt() throws IOException {
-        long n = 0;
-        loop: while (true) {
-            switch (input.peek()) {
-            case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
-                n = 10 * n + Character.digit(input.read(), 10);
-                break;
-            default: break loop;
-            }
-        }
-        return n;
     }
 }
